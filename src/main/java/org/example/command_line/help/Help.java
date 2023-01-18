528ba00d2d02a08502e2e6d792e31721be8aa449
Denis Ryzhkov <dgryzhkov@gmail.com>
2023-01-18 15:19:38 +0700

    README

--- src/main/java/org/example/command_line/help/Help.java
+++ src/main/java/org/example/command_line/help/Help.java
@@ -6,16 +6,16 @@ package org.example.command_line.help;
 public class Help {
     public static void displayHelp(){
         System.out.printf("%nСПРАВКА: %n" +
-                "java -jar <путь к merge_sort.jar> <тип данных> <имя выходного файла> <имя входного файла> <режим сортировки> %n" +
+                "java -jar <путь к mergeSortCFT.jar> <тип данных> <имя выходного файла> <имя входного файла> <режим сортировки> %n" +
                 "Параметры программы задаются при запуске через аргументы командной строки, по порядку:%n" +
                 "1. режим сортировки: -a (--asc) или -d (--desc), необязательный, по умолчанию сортируем по возрастанию;%n" +
                 "2. тип данных: -s (--string) или -i (--integer), обязательный;%n" +
                 "3. имя выходного файла, обязательное;%n" +
                 "4. остальные параметры - имена входных файлов, не менее одного.%n" +
                 "%nПримеры запуска из командной строки для Windows:%n" +
-                "java -jar merge_sort.jar -i -a out.txt in1.txt in2.txt (для целых чисел по возрастанию)%n" +
-                "java -jar merge_sort.jar -s out.txt in1.txt in2.txt in3.txt (для строк по возрастанию)%n" +
-                "java -jar merge_sort.jar -d -s out.txt in1.txt in2.txt (для строк по убыванию)%n");
+                "java -jar mergeSortCFT.jar -i -a out.txt in1.txt in2.txt (для целых чисел по возрастанию)%n" +
+                "java -jar mergeSortCFT.jar -s out.txt in1.txt in2.txt in3.txt (для строк по возрастанию)%n" +
+                "java -jar mergeSortCFT.jar -d -s out.txt in1.txt in2.txt (для строк по убыванию)%n");
     }
 
     public static void displayCommandHelp(){
